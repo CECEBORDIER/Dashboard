18c368e533325974502cb9e94084f6e2f9fd145f
CECEBORDIER <33873099+CECEBORDIER@users.noreply.github.com>
2021-01-16 13:46:11 +0100

    'mongodb connection'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, MongooseModule.forRoot('mongodb+srv://Cedric:bordier@cluster0.lirpr.mongodb.net/users?retryWrites=true&w=majority')],
   controllers: [],
   providers: [],
 })
